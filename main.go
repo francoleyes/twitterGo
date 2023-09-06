95911462db7f1a3a679785bbe4623ca976adf7eb
Franco Leyes <franco.leyes@aden.org>
2023-09-06 01:25:38 -0300

    Primer commit, conexiones a AWS, DB y handlers

---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"context"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	lambda "github.com/aws/aws-lambda-go/lambda"
+	"github.com/francoleyes/twitterGo/awsgo"
+	"github.com/francoleyes/twitterGo/bd"
+	"github.com/francoleyes/twitterGo/handlers"
+	"github.com/francoleyes/twitterGo/models"
+	"github.com/francoleyes/twitterGo/secretmanager"
+)
+
+func main() {
+	lambda.Start(EjecutoLambda)
+}
+
+func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error){
+	var res *events.APIGatewayProxyResponse
+
+	awsgo.InicializoAWS()
+
+	if !ValidoParametros() {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body: "Error en las variables de entorno.",
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+
+		return res, nil
+	}
+
+	SecretModel, err := secretmanager.GetSecret(os.Getenv("SecretName"))
+	if err != nil {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body: "Error en la lectura de Secret "+err.Error(),
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+		return res, nil
+	}
+
+	path := strings.Replace(request.PathParameters["twitter"], os.Getenv("UrlPrefix"), "", -1)
+
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("path"), path)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("method"), request.HTTPMethod)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("user"), SecretModel.Username)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("password"), SecretModel.Password)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("host"), SecretModel.Host)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("database"), SecretModel.Database)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("jwtSign"), SecretModel.JWTSign)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("body"), request.Body)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("bucketName"), os.Getenv("BucketName"))
+
+	// Conecto BD
+	err = bd.ConectarBD(awsgo.Ctx)
+	if err != nil {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body: "Error conexión BD " + err.Error(),
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+		return res, nil
+	}
+
+
+	respAPI := handlers.Manejadores(awsgo.Ctx, request)
+	if respAPI.CustomResp == nil {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: respAPI.Status,
+			Body: respAPI.Message,
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+		return res, nil
+	} else {
+		return respAPI.CustomResp, nil
+	}
+
+}
+
+func ValidoParametros() bool {
+	_, traeParametroSecretName := os.LookupEnv("SecretName")
+	if !traeParametroSecretName{
+		return traeParametroSecretName
+	}
+
+	_, traeParametroBucketName := os.LookupEnv("BucketName")
+	if !traeParametroBucketName{
+		return traeParametroBucketName
+	}
+
+	_, traeParametroUrlPrefix := os.LookupEnv("UrlPrefix")
+	if !traeParametroUrlPrefix{
+		return traeParametroUrlPrefix
+	}
+
+	return traeParametroUrlPrefix
+}
---
+++ secretmanager/sm.go
@@ -0,0 +1,28 @@
+package secretmanager
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"github.com/francoleyes/twitterGo/awsgo"
+	"github.com/francoleyes/twitterGo/models"
+)
+
+func GetSecret(secretName string) (models.Secret, error) {
+	var datosSecret models.Secret
+	fmt.Println("> Pido secreto " + secretName)
+
+	svc := secretsmanager.NewFromConfig(awsgo.Cfg)
+	clave, err := svc.GetSecretValue(awsgo.Ctx, &secretsmanager.GetSecretValueInput{
+		SecretId: aws.String(secretName),
+	})
+	if err != nil {
+		fmt.Println(err.Error())
+		return datosSecret, err
+	}
+
+	json.Unmarshal([]byte(*clave.SecretString), &datosSecret)
+	fmt.Println("> Lectura secret OK " + secretName)
+	return datosSecret, nil
+}
